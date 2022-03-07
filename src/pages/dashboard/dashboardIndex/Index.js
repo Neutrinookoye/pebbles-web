81db1b3af050cdd5a5596992cd2868bb2e921326
Favour Okoye <fokoye@excitepanacea.com>
2022-03-07 23:00:46 +0100

    doneee

--- src/pages/dashboard/dashboardIndex/Index.js
+++ src/pages/dashboard/dashboardIndex/Index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Outlet } from 'react-router-dom'
-import DashboardLayout from './layout/DashboardLayout'
+import DashboardLayout from '../layout/DashboardLayout'
 
 function Index(props) {
 	const userLogin = useSelector((state) => state.userLogin)
